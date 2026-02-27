5a92c1b57f44d65caf93e561aeb78f0122776e5e
Abhi Chauhan <abhichauhan1101@gmail.com>
2026-02-27 14:24:53 +0530

    added delete request to delete a book by id

--- backend/index.js
+++ backend/index.js
@@ -84,7 +84,6 @@ app.put("/books/:id", async (req , res)=>{
         }
         
         const {id} = req.params;
-
         const result = await Book.findByIdAndUpdate(id,req.body);
 
         if(!result){
@@ -100,7 +99,24 @@ app.put("/books/:id", async (req , res)=>{
 
 })
 
+// Route to Delete a Book
+app.delete("/books/:id", async (req , res)=>{
+    try {
 
+        const {id} = req.params;
+        const result = await Book.findByIdAndDelete(id);
+
+        if(!result){
+            return res.status(404).json({message : "Book not found"});
+        }
+        
+        res.status(200).json({message : "Book deleted Successfully"})
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ message: error.message });
+    }
+})
 
 
 
